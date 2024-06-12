1a6254e00e87f275091db5f4ac124f43e8e7c56f
Dani Romo <dev@daniromo.me>
2024-06-11 20:44:02 -0600

    Added decimal & hex colors

---
+++ models/color.ts
@@ -0,0 +1,15 @@
+export const HEX_COLOR = {
+  PRIMARY: '#8bcce9',
+  TERTIARY: '#5260ff',
+  SUCCESS: '#2dd36f',
+  WARNING: '#ffc409',
+  DANGER: '#eb445a'
+} as const
+
+export const DECIMAL_COLOR = {
+  PRIMARY: 9161961,
+  TERTIARY: 5398783,
+  SUCCESS: 3003247,
+  WARNING: 16761865,
+  DANGER: 15418458
+} as const
