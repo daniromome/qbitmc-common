0f4641ce8781a6f10cbb4d9b03d513b871495233
Dani Romo <dev@daniromo.me>
2024-06-13 08:47:57 -0600

    Added DRAFT to Visibility Enum

--- models/visibility.ts
+++ models/visibility.ts
@@ -4,7 +4,8 @@ export const VISIBILITY = {
   PUBLIC: 'public',
   UNLISTED: 'hidden',
   PRIVATE: 'private',
-  RESTRICTED: 'restricted'
+  RESTRICTED: 'restricted',
+  DRAFT: 'draft'
 } as const
 
 export type Visibility = ObjectValues<typeof VISIBILITY>
